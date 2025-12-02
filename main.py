d26dc3a288c466138c7fb5c0230bff5e122abe73
Lina NZOUECHIM <linanzouechim@gmail.com>
2025-12-02 10:45:36 -0500

    Ajout des class

--- main.py
+++ main.py
@@ -2,34 +2,57 @@
 from abc import ABC
 from operator import truediv
 
-
+# class Document
 class Document(ABC):
-    def __init__(self, nom, auteur):
-        self.nom = nom
-        self.auteur = auteur
+    def __init__(self, titre):
+        self.titre = titre
 
     def __str__(self):
         return "[Document  " + self.nom + "   " + self.auteur + " ]"
 
 
-# Livres (ISBN, annee)
-class Livre(Document):
-    def __init__(self, nom, auteur, ISBN, annee):
+
+# class Journal
+class Journal(Document):
+
+    def __init__(self, date_parution):
+        self.date_parution = date_parution
+
+# class volume
+class Volume(Document):
+    def __init__(self, nom_auteur):
+        self.nom_auteur = nom_auteur
+
+# class Livres (ISBN, annee)
+class Livre(Volume):
+    def __init__(self):
         super().__init__(nom, auteur)
         self.ISBN = ISBN
         self.annee = annee
 
-# Adherent id, nom, prenom,
-# identifiant unique ...
+# class BD
+class BD(Volume):
+    def __init__(self, dessinateur):
+        self.dessinateur = dessinateur
 
+# class Dictionnaire
+class Dictionnaire(Volume):
+
+
+# class Adherent
 class Adherent:
-    pass
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
 
 
-# Emprunt (livre, adherent)
 
+# class Emprunt (livre, adherent)
 class Emprunt:
-    pass
+    def __init__(self, livre, adherent, date_emprunt, date_retour):
+        self.date_emprunt = date_emprunt
+        self.date_retour = date_retour
+
 
 
 # Bibliotheque :
@@ -37,6 +60,7 @@ class Emprunt:
 # liste d'adherants
 # liste d'emprunts
 class Bibliotheque:
+    listeDocuments = []
     listeLivres = []
     listeEmprunts = []
     listeAdherents = []
@@ -44,12 +68,22 @@ class Bibliotheque:
     def charger_livres_depuis_fichier(cls):
         pass
 
+# méthode ajouter document
+    def  ajouter_document(self):
+
+# méthode ajouter adhérent
+    def ajoiuter_adherent(self):
+
+
+# méthode supprimer adhérent
+    def  supprimmer_adherent(self):
+
     @classmethod
     def afficher_liste_livres(cls):
         for l in cls.listeLivres:
             print(l)
 
-
+# c
 # GestionFichier
 
 # Menu
@@ -68,11 +102,11 @@ class Menu:
         print("* 3 Afficher tous les adhérents               *")
         print("* 4 Ajouter Document                          *")
         print("* 5 Supprimer Document                        *")
-        print("* 6  Afficher tous les Documents              *")
-        print("* 7  Ajouter Emprunt                          *")
-        print("* 8  Retour d’un Emprunt                      *")
-        print("* 9  Afficher tous les Emprunts               *")
-        print("* 10  Quitter                                 *")
+        print("* 6 Afficher tous les Documents               *")
+        print("* 7 Ajouter Emprunt                           *")
+        print("* 8 Retour d’un Emprunt                       *")
+        print("* 9 Afficher tous les Emprunts                *")
+        print("* 10 Quitter                                  *")
         print("*                                             *")
         print("***********************************************")
 # Ajout adhérent
@@ -86,8 +120,7 @@ condition = True
 while (condition):
     Menu.afficherMenu()
     reponse = int(input("Reponse : "))
-    reponse = reponse.lower()
-    if (reponse == "c"):
+    if (reponse == 1):
         condition = True
     elif (reponse == "q"):
         condition = False
